970db3d160dc7c8467208df26d27e332e958fa4c
Sophie Houben <mariesophie.houben@gmail.com>
2023-09-07 18:28:56 +0200

    add event listeners to cells

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+function createPlayer({name, symbol}){
+    return {
+        name,
+        symbol
+    };
+  }
+
+
+var gameFlow = (function(){
+    'use strict';
+    const player1 = createPlayer({name: 'Anna', symbol: 'X'});
+    const player2 = createPlayer({name: 'Isa', symbol: 'O'});
+    let currentPlayer = player1;
+
+    return{
+        currentPlayer
+    };
+    
+})();
+
 var gameBoard = (function() {
     'use strict';
     const boardModule = (function() {
         const board = {
-            boardArray: ['x','o','x','o','x','o','x','o','x']
+            boardArray: ['','','','','','','','','']
         };
 
         function renderBoard(){
@@ -17,8 +37,22 @@ var gameBoard = (function() {
                 }
             }
         }
-        
+
+        function addCellEventListeners(){
+            const table = document.getElementById('boardTable');
+            const rows = table.querySelectorAll('tr');
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    rows[i].children[j].addEventListener('click', function(event){
+                        const index = parseInt(event.target.getAttribute('data-index'), 10);
+                        board.boardArray[index] = gameFlow.currentPlayer.symbol;
+                        renderBoard();
+                    })
+                }
+            }
+        }
         renderBoard();
+        addCellEventListeners();
     
         return {
             getArray: function() {
@@ -29,17 +63,3 @@ var gameBoard = (function() {
 
     return boardModule; 
 })();
-
-function createPlayer({name, symbol}){
-    return {
-        name
-    };
-  }
-
-var gameFlow = (function(){
-    'use strict';
-    
-})
-
-
-
