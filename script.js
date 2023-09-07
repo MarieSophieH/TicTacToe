0aca512f31c4fae3bb75f21d5279a93c8baddb47
Sophie Houben <mariesophie.houben@gmail.com>
2023-09-07 17:24:10 +0200

    Add function to render the gameboard

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ var gameBoard = (function() {
     'use strict';
     const boardModule = (function() {
         const board = {
-            boardArray: []
+            boardArray: ['x','o','x','o','x','o','x','o','x']
         };
 
-       
+        function renderBoard(){
+            const table = document.getElementById('boardTable');
+            const rows = table.querySelectorAll('tr');
+            
+            let index = 0;
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    rows[i].children[j].textContent = board.boardArray[index];
+                    index++;
+                }
+            }
+        }
+        
+        renderBoard();
+    
         return {
             getArray: function() {
                 return board.boardArray;
