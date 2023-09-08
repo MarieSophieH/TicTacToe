949cabb0fb881c56ed8e31a4f4daea94f02984bc
Sophie Houben <mariesophie.houben@gmail.com>
2023-09-08 14:53:54 +0200

    implement game logic

--- script.js
+++ script.js
@@ -56,12 +56,40 @@ var gameBoard = (function() {
                                 currentPlayer = gameFlow.player1;
                             }
                         }
-                        
                         renderBoard();
+                        checkLogic();
                     })
                 }
             }
         }
+
+        function checkLogic(){
+                for(let i = 0; i < 3; i++){
+                    let j = 0;
+                    if ((board.boardArray[i][j] == board.boardArray[i][j+1]) && (board.boardArray[i][j]== board.boardArray[i][j+2]) && board.boardArray[i][0] != ""){
+                        console.log("Winner");
+                        return true;
+                    }
+                }
+                for(let j = 0; j < 3; j++){
+                    let i = 0;
+                    if ((board.boardArray[i][j] == board.boardArray[i+1][j]) && (board.boardArray[i][j] == board.boardArray[i+2][j]) && board.boardArray[0][j] != ""){
+                        console.log("Winner");
+                        return true;
+                    }
+                }
+                if((board.boardArray[0][0] == board.boardArray[1][1]) && (board.boardArray[0][0] == board.boardArray[2][2]) && (board.boardArray[0][0] != "")){
+                    console.log("Winner");
+                    return true;
+                }
+                if((board.boardArray[2][0] == board.boardArray[1][1]) && (board.boardArray[2][0] == board.boardArray[0][2]) && (board.boardArray[2][0] != "")){
+                    console.log("Winner");
+                    return true;
+                }
+
+                return false;
+        }
+
         renderBoard();
         addCellEventListeners();
     
